f02eb8e26f4eecbfd1eeb3f1d8b06205dd489d60
Retford <retfordjb@gmail.com>
2025-04-04 23:34:35 -0500

    header and footer removed

--- src/MiniApp.tsx
+++ src/MiniApp.tsx
@@ -1,13 +1,9 @@
-import { Footer } from './components/ui/Footer';
-import { Header } from './components/ui/Header';
 import { Main } from './components/ui/Main';
 
 export const MiniApp = () => {
   return (
-    <div className='grid place-content-center min-h-screen grid-rows-[auto_1fr_auto]'>
-      <Header />
+    <div className='grid place-content-center min-h-screen'>
       <Main />
-      <Footer />
     </div>
   );
 };
