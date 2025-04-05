2075c7e0c953376b4cb0eefc82e2ddd343eac247
Retford <retfordjb@gmail.com>
2025-04-05 11:51:32 -0500

    height size modified

--- src/MiniApp.tsx
+++ src/MiniApp.tsx
@@ -2,7 +2,7 @@ import { Main } from './components/ui/Main';
 
 export const MiniApp = () => {
   return (
-    <div className='grid place-content-center min-h-screen'>
+    <div className='grid place-content-center min-h-svh'>
       <Main />
     </div>
   );
